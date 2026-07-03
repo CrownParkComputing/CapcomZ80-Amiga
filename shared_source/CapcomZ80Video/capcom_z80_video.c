12e7ecba2b7e8f36316408ebf87a39915a690309
CrownParkComputing <jonnyb666@hotmail.co.uk>
2026-07-03 19:00:22 +0100

    Inline hot vertical video helpers

--- shared_source/CapcomZ80Video/capcom_z80_video.c
+++ shared_source/CapcomZ80Video/capcom_z80_video.c
@@ -1,22 +1,4 @@
 #include "capcom_z80_video.h"
-#include <string.h>
-
-int capcom_z80_bit(const unsigned char *p, unsigned bit)
-{
-    return (p[bit >> 3] >> (7 - (bit & 7))) & 1;
-}
-
-int capcom_z80_weight4(int v)
-{
-    v &= 0xf;
-    return 0x0e * (v & 1) + 0x1f * ((v >> 1) & 1) +
-           0x43 * ((v >> 2) & 1) + 0x8f * ((v >> 3) & 1);
-}
-
-uint8_t capcom_z80_rgb332(unsigned r, unsigned g, unsigned b)
-{
-    return (uint8_t)(((r >> 5) << 5) | ((g >> 5) << 2) | (b >> 6));
-}
 
 void capcom_z80_palette_rgb332_weighted4(const unsigned char *proms, uint8_t *out)
 {
@@ -33,26 +15,6 @@ void capcom_z80_palette_rgb888_linear4(const unsigned char *proms, uint8_t *out)
         out[i * 3 + 1] = (uint8_t)((proms[0x100 + i] & 0xf) * 17);
         out[i * 3 + 2] = (uint8_t)((proms[0x200 + i] & 0xf) * 17);
     }
-}
-
-void capcom_z80_clear_frame(uint8_t *frame, uint8_t pen)
-{
-    memset(frame, pen, CAPCOM_Z80_FRAME_SIZE);
-}
-
-uint8_t *capcom_z80_rotated_ptr(uint8_t *frame, int absx, int absy)
-{
-    int x = absy - CAPCOM_Z80_ROT_YOFF;
-    int y = 255 - absx;
-    if ((unsigned)x >= CAPCOM_Z80_GAME_W || (unsigned)y >= CAPCOM_Z80_GAME_H)
-        return 0;
-    return frame + (size_t)y * CAPCOM_Z80_GAME_W + x;
-}
-
-void capcom_z80_put_rotated(uint8_t *frame, int absx, int absy, uint8_t pen)
-{
-    uint8_t *p = capcom_z80_rotated_ptr(frame, absx, absy);
-    if (p) *p = pen;
 }
 
 void capcom_z80_scale_rgb332(const uint8_t *src, const uint8_t *palette,
--- shared_source/CapcomZ80Video/capcom_z80_video.h
+++ shared_source/CapcomZ80Video/capcom_z80_video.h
@@ -2,6 +2,7 @@
 #define CAPCOM_Z80_VIDEO_H
 
 #include <stdint.h>
+#include <string.h>
 
 #define CAPCOM_Z80_SRC_W 256
 #define CAPCOM_Z80_SRC_H 224
@@ -10,14 +11,45 @@
 #define CAPCOM_Z80_ROT_YOFF 16
 #define CAPCOM_Z80_FRAME_SIZE (CAPCOM_Z80_GAME_W * CAPCOM_Z80_GAME_H)
 
-int capcom_z80_bit(const unsigned char *p, unsigned bit);
-int capcom_z80_weight4(int v);
-uint8_t capcom_z80_rgb332(unsigned r, unsigned g, unsigned b);
+static inline int capcom_z80_bit(const unsigned char *p, unsigned bit)
+{
+    return (p[bit >> 3] >> (7 - (bit & 7))) & 1;
+}
+
+static inline int capcom_z80_weight4(int v)
+{
+    v &= 0xf;
+    return 0x0e * (v & 1) + 0x1f * ((v >> 1) & 1) +
+           0x43 * ((v >> 2) & 1) + 0x8f * ((v >> 3) & 1);
+}
+
+static inline uint8_t capcom_z80_rgb332(unsigned r, unsigned g, unsigned b)
+{
+    return (uint8_t)(((r >> 5) << 5) | ((g >> 5) << 2) | (b >> 6));
+}
+
+static inline void capcom_z80_clear_frame(uint8_t *frame, uint8_t pen)
+{
+    memset(frame, pen, CAPCOM_Z80_FRAME_SIZE);
+}
+
+static inline uint8_t *capcom_z80_rotated_ptr(uint8_t *frame, int absx, int absy)
+{
+    int x = absy - CAPCOM_Z80_ROT_YOFF;
+    int y = 255 - absx;
+    if ((unsigned)x >= CAPCOM_Z80_GAME_W || (unsigned)y >= CAPCOM_Z80_GAME_H)
+        return 0;
+    return frame + (unsigned)y * CAPCOM_Z80_GAME_W + x;
+}
+
+static inline void capcom_z80_put_rotated(uint8_t *frame, int absx, int absy, uint8_t pen)
+{
+    uint8_t *p = capcom_z80_rotated_ptr(frame, absx, absy);
+    if (p) *p = pen;
+}
+
 void capcom_z80_palette_rgb332_weighted4(const unsigned char *proms, uint8_t *out);
 void capcom_z80_palette_rgb888_linear4(const unsigned char *proms, uint8_t *out);
-void capcom_z80_clear_frame(uint8_t *frame, uint8_t pen);
-uint8_t *capcom_z80_rotated_ptr(uint8_t *frame, int absx, int absy);
-void capcom_z80_put_rotated(uint8_t *frame, int absx, int absy, uint8_t pen);
 void capcom_z80_scale_rgb332(const uint8_t *src, const uint8_t *palette,
                              uint8_t *dst, int dst_stride, int dst_w, int dst_h);
 
